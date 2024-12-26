8b706df7fe1e8279e0d2a8dc9b5b0060d41e47c6
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-26 04:13:07 -0800

    Add project files.

---
+++ Post.Common/Events/Events.cs
@@ -0,0 +1,22 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public record CommentAddedEvent(
+    Guid CommentId,
+    string Comment,
+    string UserName,
+    DateTime CommentDateTime
+    ) : EventBase(nameof(CommentAddedEvent));
+
+public record CommentRemovedEvent(Guid CommentId) : EventBase(nameof(CommentRemovedEvent));
+
+public record CommentUpdatedEvent(Guid CommentId, string Comment, string UserName, DateTime EditDateTime) : EventBase(nameof(CommentUpdatedEvent));
+
+public record MessageUpdatedEvent(string Message) : EventBase(nameof(MessageUpdatedEvent));
+
+public record PostCreatedEvent(string Author, string Message) : EventBase(nameof(PostCreatedEvent));
+
+public record PostRemovedEvent() : EventBase(nameof(PostRemovedEvent));
+
+public record PostLikedEvent() : EventBase(nameof(PostLikedEvent));
