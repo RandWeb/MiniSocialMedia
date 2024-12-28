8531819a86d626328300d6d1765805906c8c7ce5
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-28 11:53:03 -0800

    افزودن کانتکست

---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Infrastructure.DataAccess;
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+    public DbSet<PostEntity> Posts { get; set; }
+    public DbSet<CommentEntity> Comments { get; set; }
+/*    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PostEntity>().HasKey(x => x.PostId);
+        modelBuilder.Entity<CommentEntity>().HasKey(x => x.CommentId);
+        modelBuilder.Entity<PostEntity>().HasMany(x => x.Comments).WithOne(x => x.Post).HasForeignKey(x => x.PostId);
+    }*/
+}
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Post.Query.Infrastructure.DataAccess;
+
+public class DatabaseContextFactory
+{
+    private readonly Action<DbContextOptionsBuilder> optionsBuilder;
+
+    public DatabaseContextFactory(Action<DbContextOptionsBuilder> optionsBuilder)
+    {
+        this.optionsBuilder = optionsBuilder;
+    }
+
+    public DatabaseContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<DatabaseContext>();
+        optionsBuilder(options);
+
+        return new DatabaseContext(options.Options);
+    }
+
+}
