4252ead36d72d8328b645b0b6e9cb71b9e6ec89a
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-28 11:51:44 -0800

    افزودن موجودیت های دامین

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities;
+
+[Table("Comments")]
+public class CommentEntity
+{
+    [Key]
+    public Guid CommentId { get; set; }
+    public string UserName { get; set; }
+
+    public string Comment { get; set; }
+    public DateTime Created_At { get; set; }
+
+    public bool IsEdited { get; set; }
+
+    public Guid PostId { get; set; }
+
+    [System.Text.Json.Serialization.JsonIgnore]
+    public virtual PostEntity Post { get; set; }
+}
