cd5230137d53022e13b7044e746c24e86125c033
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-28 11:52:18 -0800

    افزودن ریپوزیتوری های لایه دامین

---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+
+public interface ICommentRepository
+{
+    Task CreateAsync(CommentEntity comment);
+    Task UpdateAsync(CommentEntity comment);
+    Task DeleteAsync(Guid commentId);
+    Task<CommentEntity> GetCommentByIdAsync(Guid commentId);
+    Task<List<CommentEntity>> GetCommentsAsync();
+    Task<List<CommentEntity>> GetCommentsByUserNameAsync(string username);
+    Task<List<CommentEntity>> GetCommentsByPostIdAsync(Guid postId);
+}
---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+public interface IPostRepository
+{
+    Task CreateAsync(PostEntity post);
+    Task DeleteAsync(Guid postId);
+    Task UpdateAsync(PostEntity post);
+
+    Task<PostEntity> GetPostByIdAsync(Guid postId);
+
+    Task<List<PostEntity>> GetPostsAsync();
+
+    Task<List<PostEntity>> GetPostsByAuthorAsync(string author);
+
+    Task<List<PostEntity>> GetPostsWithLikesAsync(int numberOfLikes);
+
+    Task<List<PostEntity>> GetPostsWithCommentsAsync();
+}
