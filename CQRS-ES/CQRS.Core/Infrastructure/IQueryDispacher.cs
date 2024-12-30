11d70ec534f0606a62a617c3054ff1a52d1c1854
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-30 07:27:46 -0800

    افزودن dispacher

---
+++ CQRS-ES/CQRS.Core/Infrastructure/IQueryDispacher.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Queries;
+
+namespace CQRS.Core.Infrastructure;
+public interface IQueryDispacher<TEntity>
+{
+    void RegisterAsync<TQuery>(Func<TQuery,Task<List<TEntity>>> handler)  where TQuery : QueryBase;
+    Task<List<TEntity>> SendAsync<TQuery>(TQuery query) where TQuery : QueryBase;
+}
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Dispatchers/QueryDispacher.cs
@@ -0,0 +1,26 @@
+﻿using CQRS.Core.Infrastructure;
+using CQRS.Core.Queries;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Infrastructure.Dispatchers;
+public sealed class QueryDispacher : IQueryDispacher<PostEntity>
+{
+    private readonly Dictionary<Type, Func<QueryBase, Task<List<PostEntity>>>> _handlers = [];
+    public void RegisterAsync<TQuery>(Func<TQuery, Task<List<PostEntity>>> handler) where TQuery : QueryBase
+    {
+        if (_handlers.ContainsKey(typeof(TQuery)))
+        {
+            throw new InvalidOperationException($"Handler for query {typeof(TQuery).Name} already registered.");
+        }
+        _handlers.Add(typeof(TQuery), query => handler((TQuery)query));
+    }
+
+    public async Task<List<PostEntity>> SendAsync<TQuery>(TQuery query) where TQuery : QueryBase
+    {
+        if (!_handlers.ContainsKey(typeof(TQuery)))
+        {
+            throw new InvalidOperationException($"Handler for query {typeof(TQuery).Name} not registered.");
+        }
+        return await _handlers[typeof(TQuery)](query);
+    }
+}
