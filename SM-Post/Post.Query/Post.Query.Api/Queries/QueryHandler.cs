6b674aaf097275b2a2606aaf47d7bea90905df1f
مهرداد تابش <mehrdadit12@gmail.com>
2024-12-30 07:29:13 -0800

    افزودن کوئری هندلر ها

---
+++ SM-Post/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries;
+
+internal sealed class QueryHandler(IPostRepository postRepository) : IQueryHandler
+{
+    private readonly IPostRepository _postRepository = postRepository;
+
+    public async Task<List<PostEntity>> HandleAsync(FindAllPostsQuery query)
+    {
+        return await _postRepository.GetPostsAsync();
+    }
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+    {
+        return [await _postRepository.GetPostByIdAsync(query.Id)];
+    }
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostsByAuthorQuery query)
+    {
+        return await _postRepository.GetPostsByAuthorAsync(query.Author);
+    }
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostsWithCommentsQuery query)
+    {
+        return await _postRepository.GetPostsWithCommentsAsync();
+    }
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostsWithLikesQuery query)
+    {
+        return await _postRepository.GetPostsWithLikesAsync(query.NumberOfLikes);
+    }
+}
